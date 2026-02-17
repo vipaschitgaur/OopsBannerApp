eaa0d1a933552758ce677318921bdfcdf2ccfb9b
ReallyyTrash <navyaattri6@gmail.com>
2026-02-17 11:20:21 +0530

    UC 3 String.join
    Updated the code to correc the memory and string manipulation from using the operator to using the string.join funciton

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,11 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        String line1 = "  ***  " + "   ***  " + " ***** " + "   **** ";
-        String line2 = " *   * " + "  *   * " + " *    *" + "  *     ";
-        String line3 = " *   * " + "  *   * " + " *    *" + "   ***  ";
-        String line4 = " *   * " + "  *   * " + " ***** " + "      * ";
-        String line5 = " *   * " + "  *   * " + " *     " + "      * ";
-        String line6 = "  ***  " + "   ***  " + " *     " + "  ****  ";
+        String line1 = String.join("", "  ***  ", "   ***  ", " ***** ", "   **** ");
+        String line2 = String.join("", " *   * ", "  *   * ", " *    *", "  *     ");
+        String line3 = String.join("", " *   * ", "  *   * ", " *    *", "   ***  ");
+        String line4 = String.join("", " *   * ", "  *   * ", " ***** ", "      * ");
+        String line5 = String.join("", " *   * ", "  *   * ", " *     ", "      * ");
+        String line6 = String.join("", "  ***  ", "   ***  ", " *     ", "  ****  ");
 
         System.out.println(line1);
         System.out.println(line2);
